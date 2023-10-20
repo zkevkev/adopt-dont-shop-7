859b4d72d61e881b1eb7aa9f015a152a00ee59b9
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-20 14:14:49 -0600

    Feat:4US Write incomplete ::search_by_name in pet model and tests

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -12,4 +12,11 @@ class Pet < ApplicationRecord
   def self.adoptable
     where(adoptable: true)
   end
+
+  def self.search_by_name(name)
+    if name.present?
+      @pets = Pet.search(name)
+    end
+    redirect_to "/applications/#{}"
+  end
 end
