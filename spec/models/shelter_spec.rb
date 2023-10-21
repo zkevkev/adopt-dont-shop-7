299ea8f54340e632abefc65396d0dc593ccb0801
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-21 15:38:53 -0600

    Feat:10US Implement basic structure for relationship between admin and shelters

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe Shelter, type: :model do
   describe "relationships" do
     it { should have_many(:pets) }
+    it { should belong_to(:admin) }
   end
 
   describe "validations" do
