51abbc5d3443edf418157692ba9e8ab3f3038901
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-21 21:23:16 -0600

    Feat:12US Write tests for admin applications show feature

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe "application show" do
   before (:each) do
     admin = Admin.create
     @shelter = admin.shelters.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    @application_1 = admin.applications.create(name: "Bob", address: "123 1st St", city: "Pleasantville", state: "CO", zipcode: 80501, description: "they're cute", status: "In Progress")
+
     @pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: @shelter.id)
     @pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: @shelter.id)
     @pet_3 = Pet.create(adoptable: false, age: 2, breed: "saint bernard", name: "Beethoven", shelter_id: @shelter.id)
     @pet_4 = Pet.create(adoptable: false, age: 5, breed: "bernese", name: "Mr. Beet", shelter_id: @shelter.id)
-    @application_1 = admin.applications.create(name: "Bob", address: "123 1st St", city: "Pleasantville", state: "CO", zipcode: 80501, description: "they're cute", status: "In Progress")
     
     @pet_1.applications << @application_1
     @pet_2.applications << @application_1
