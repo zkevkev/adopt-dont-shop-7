b3ded1e31bf1c4d6ced5c708c52e36dfcca19f3d
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-24 14:43:39 -0600

    Ref: Admin_shelters controller and index for clarity

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,11 +1,5 @@
 class AdminSheltersController < ApplicationController
   def index
-    @shelters_pending = Shelter.all
-
-
-    # @applications = Application.all 
     @shelters = Shelter.order_by_name
   end
-
-
 end
