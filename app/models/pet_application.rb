483c82f4c5655c25559e9158961937c02bc34a6e
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-25 09:25:12 -0600

    Ref: Add seeds to db

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,7 +2,7 @@ class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
 
-  def self.find_pet_app(pet_id, application_id)
-    PetApplication.where("pet_id = #{pet_id}", "application_id = #{application_id}")
-  end
+  # def self.find_pet_app(pet_id, application_id)
+  #   PetApplication.where("pet_id = #{pet_id}", "application_id = #{application_id}")
+  # end
 end
